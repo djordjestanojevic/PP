b755f3ddb477d97cd5add0d56cb4a82c97683b10
djordjestanojevic <33425991+djordjestanojevic@users.noreply.github.com>
2018-05-27 17:46:46 +0200

    Update njutnova_metoda_tangente.hs

--- njutnova_metoda_tangente.hs
+++ njutnova_metoda_tangente.hs
@@ -1,7 +1,13 @@
 import Diferenciranje as D
+import Simple as S
 --pronalazi nulu eksplicitno zadate funkcije f na intervalu a b sa tacnoscu eps
 njutn_resenje:: Double->Double->(Double->Double)->Double->Double
-njutn_resenje a b f eps = njutn_pom a b f eps b
+njutn_resenje a b f eps |(f a)*(f b) > 0 = error "Funkcija nije lokalizovana"
+                        |S.ismonotonic (map (f) niz) == False = error "Funkcija nije monotona"
+                        |S.ismonotonic (niz_izvoda niz (map f niz) niz) == False = error "Drugi izvod nije konstantan"
+                        |(f b)*(D.izvod b niz (niz_izvoda niz (map f niz) niz))>0 = njutn_pom a b f eps b
+                        |otherwise = njutn_pom a b f eps a
+                        where niz = [a,a+(b-a)/10..b]
 
 --funkcija sa zadatim pocetnim cvorom
 njutn_pom:: Double->Double->(Double->Double)->Double->Double->Double
@@ -14,10 +20,15 @@ modifikacija:: Double->Double->(Double->Double)->Double->Int->Double
 modifikacija a b f eps k = mod_pom a b f eps b k 0 0
 
 
-mod_pom::  Double->Double->(Double->Double)->Double->Int->Int->Double->Double
+mod_pom::  Double->Double->(Double->Double)->Double->Double->Int->Int->Double->Double
 mod_pom a b f eps xn k 0 i | abs((f xn)/(iz))<eps =xn
                              | otherwise = mod_pom a b f eps (xn - ((f xn)/(iz))) k k (iz)
     where iz = D.izvod xn ([a,a+((b-a)/10)..b]) (map f ([a,a+((b-a)/10)..b]))
 mod_pom a b f eps xn k p i | abs(((f xn)/(i)))<eps =xn
                              | otherwise = mod_pom a b f eps (xn - ((f xn)/(i))) k (p-1) i
 
+
+--pravi niz vrednosti izvoda funkcije f
+niz_izvoda::[Double]->[Double]->[Double]->[Double]
+niz_izvoda [] _ _ = []
+niz_izvoda x f x0 = (D.izvod (head x) x0 f):(niz_izvoda (tail x) f x0)
